b7397468a148cd99c46c5f2cad992d0101f937cf
Joan Molinas Ramon <joanmramon@gmail.com>
2017-07-24 12:47:14 +0200

    [CORE] SignInViewController border added and enter button width adjusted

--- Hamp/Hamp/ViewControllers/Base View Controllers/LogoTitleBaseViewController.swift
+++ Hamp/Hamp/ViewControllers/Base View Controllers/LogoTitleBaseViewController.swift
@@ -12,12 +12,23 @@ class LogoTitleBaseViewController: HampViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.white
+        addImageToTitle()
+//        addBottomLayer()
+    }
+    
+    private func addImageToTitle() {
         let image = #imageLiteral(resourceName: "SmallBag")
         let imageView = UIImageView(image: image)
         imageView.frame = CGRect(x: 0, y: 0, width: 27, height: 34)
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
-        
-        navigationController?.navigationBar.barTintColor = UIColor.white
+    }
+    
+    private func addBottomLayer() {
+        let layer = CALayer()
+        layer.frame = CGRect(x: 0, y: navigationController!.navigationBar.frame.height - 1, width: navigationController!.navigationBar.frame.width, height: 1)
+        layer.backgroundColor = UIColor.darkPink.cgColor
+        navigationController!.navigationBar.layer.addSublayer(layer)
     }
 }
