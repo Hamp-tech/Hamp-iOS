f1945a7bb070610c6c368e628d7d8a44032adca0
Joan Molinas Ramon <joanmramon@gmail.com>
2017-07-25 20:53:29 +0200

    [CORE] Connect to hamp kit

--- Hamp/Hamp/AppDelegate.swift
+++ Hamp/Hamp/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        Hamp.environtment = try? HampEnvirontmentsProvider.productionEnvirontment()
+        try? Hamp.connect()
+        
         return true
     }
 }
