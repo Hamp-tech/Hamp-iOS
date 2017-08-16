ea51a488077c8e97f0fbbce3d6972ea64f907feb
Joan Molinas Ramon <joanmramon@gmail.com>
2017-08-16 12:14:39 +0200

    [CORE - UI] Services collectionview layout ui

---
+++ Hamp/Hamp/Controllers/ViewControllers/Services/ServicesCollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ServicesCollectionViewController.swift
+//  Hamp
+//
+//  Created by Joan Molinas Ramon on 16/8/17.
+//  Copyright © 2017 com.hamp.hampiOS. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class ServicesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "reuse")
+        
+    }
+}
+
+extension ServicesCollectionViewController {
+    //MARK: DataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuse", for: indexPath)
+        
+        cell.contentView.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+extension ServicesCollectionViewController: UICollectionViewDelegateFlowLayout {
+    //MARK: Layout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 10
+        let size: CGFloat = collectionView.frame.width/2 - padding*2
+        return CGSize.init(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.init(top: 10, left: 12.5, bottom: 10, right: 12.5)
+    }
+}
