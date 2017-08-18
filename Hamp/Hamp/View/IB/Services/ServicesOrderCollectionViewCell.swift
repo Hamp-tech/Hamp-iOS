693eda258291b51f567fa6b207548a414e91908e
Joan Molinas Ramon <joanmramon@gmail.com>
2017-08-18 02:05:31 +0200

    [UI] ServicesOrderViewController collectionViewCell placeholder elements

--- Hamp/Hamp/View/IB/Services/ServicesOrderCollectionViewCell.swift
+++ Hamp/Hamp/View/IB/Services/ServicesOrderCollectionViewCell.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ServicesOrderCollectionViewCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK: Properties
+    var service: Service!
+    
+    //MARK: Life cycle
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        setupSecondaryViews()
     }
-
 }
 
 extension ServicesOrderCollectionViewCell: Reusable { }
+
+private extension ServicesOrderCollectionViewCell {
+    //MARK: Private
+    func setupSecondaryViews() {
+        let auxiliarViewToShowLeftSeparator = UIView.init(frame: bounds)
+        auxiliarViewToShowLeftSeparator.cornerRadius = 9
+        auxiliarViewToShowLeftSeparator.backgroundColor = .white
+        auxiliarViewToShowLeftSeparator.layer.masksToBounds = true
+        contentView.insertSubview(auxiliarViewToShowLeftSeparator, at: 0)
+        
+        let separator = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 10, height: bounds.height))
+        separator.backgroundColor = UIColor.darkPink
+        separator.layer.masksToBounds = true
+        auxiliarViewToShowLeftSeparator.addSubview(separator)
+    }
+}
