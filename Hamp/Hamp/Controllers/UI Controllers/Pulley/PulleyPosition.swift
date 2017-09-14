ee6f2eeb41b0e8436eaf1c58dad0771f0b3d9eee
Joan Molinas Ramon <joanmramon@gmail.com>
2017-09-14 21:33:23 +0200

    [CORE] HistoryBookingDetail pulley view controller base effect

---
+++ Hamp/Hamp/Controllers/UI Controllers/Pulley/PulleyPosition.swift
@@ -0,0 +1,19 @@
+//
+//  PulleyPosition.swift
+//  Hamp
+//
+//  Created by Joan Molinas Ramon on 14/9/17.
+//  Copyright © 2017 com.hamp.hampiOS. All rights reserved.
+//
+
+import Foundation
+
+//enum PulleyPosition: Int {
+//    case collapsed = 0
+//    case open = 1
+//    
+//    static let all: [PulleyPosition] = {
+//        return [.collapsed, .open]
+//    }()
+//}
+
--- Hamp/Hamp/Controllers/ViewControllers/History/HistoryBookingDetailViewController.swift
+++ Hamp/Hamp/Controllers/ViewControllers/History/HistoryBookingDetailViewController.swift
@@ -9,68 +9,11 @@
 import UIKit
 import MapKit
 
-private enum TablePosition {
-    case up
-    case down
-}
-
-class HistoryBookingDetailViewController: HampViewController {
-    
-    // MARK: - IB Properties
-    @IBOutlet private weak var map: MKMapView!
-    @IBOutlet private weak var tableView: UITableView!
-    @IBOutlet private weak var tableViewTopConstraint: NSLayoutConstraint!
-    private var tablePosition = TablePosition.down
-    private var foo: CGFloat!
-    private lazy var tapTableViewGesture: UITapGestureRecognizer =  {
-        let tapTableViewGesture = UITapGestureRecognizer.init(target: self, action: #selector(tableWasTapped(sender:)))
-        return tapTableViewGesture
-    }()
-    
-    // MARK: - Life cycle
+class HistoryBookingDetailViewController: PulleyViewController {
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.transaparentBar()
-        
-    
-    }
-    
-    @objc func tableWasTapped(sender: UITapGestureRecognizer) {
-        let foobar: CGFloat
-        switch self.tablePosition {
-        case .up:
-            foobar = -30
-        case .down:
-            foobar = -(tableView.frame.origin.y - navigationController!.navigationBar.frame.height)
-        }
-        
-        self.tablePosition = self.tablePosition == .up ? .down : .up
-        
-        self.tableViewTopConstraint.constant = foobar
-
-    }
-
-}
-
-extension HistoryBookingDetailViewController: UITableViewDelegate {
-    
-}
-
-extension HistoryBookingDetailViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell.init()
     }
 }
 
-private extension HistoryBookingDetailViewController {
-    
-}
 
