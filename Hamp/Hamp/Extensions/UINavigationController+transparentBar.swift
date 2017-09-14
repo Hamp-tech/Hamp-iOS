0d02f56022c7ee081f87368927189f0d8e7ef1eb
Joan Molinas Ramon <joanmramon@gmail.com>
2017-09-14 16:14:58 +0200

    [CORE] Transaparent navigation bar

---
+++ Hamp/Hamp/Extensions/UINavigationController+transparentBar.swift
@@ -0,0 +1,17 @@
+//
+//  UINavigationController+transparentBar.swift
+//  Hamp
+//
+//  Created by Joan Molinas Ramon on 13/9/17.
+//  Copyright © 2017 com.hamp.hampiOS. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    func transaparentBar() {
+        self.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.isTranslucent = true
+    }
+}
