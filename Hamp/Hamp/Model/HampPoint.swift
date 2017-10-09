316f1b9f9e373a0587d3178b832c984ed8207c90
Joan Molinas Ramon <joanmramon@gmail.com>
2017-10-09 21:58:29 +0200

    [CORE] Hampoint added

---
+++ Hamp/Hamp/Model/HampPoint.swift
@@ -0,0 +1,17 @@
+//
+//  HampPoint.swift
+//  Hamp
+//
+//  Created by Joan Molinas Ramon on 9/10/17.
+//  Copyright © 2017 com.hamp.hampiOS. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+struct HampPoint {
+    let identifier: String
+    let name: String
+    let localization: CLLocationCoordinate2D
+    let postalCode: String
+}
