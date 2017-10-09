54ee690e6b8dc07c031f8c7a37e71bd57b8b046d
Aleix Baggerman <aleixdiazbaggerman@gmail.com>
2017-10-09 21:23:41 +0200

    SearchBarDelegate

---
+++ Hamp/Hamp/Controllers/HampSearchBarDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  HampSearchBarDelegate.swift
+//  Hamp
+//
+//  Created by Aleix Baggerman on 7/10/17.
+//  Copyright © 2017 com.hamp.hampiOS. All rights reserved.
+//
+
+import Foundation
+
+protocol HampSearchBarDelegate {
+    func searchBarSetInitialElements () -> [String]
+    func searchBarDidUpdateSearchElements (elements: [String])
+}
