9ae343554bf38da7379f5ea36050209fa7f412a4
Joan Molinas Ramon <joanmramon@gmail.com>
2018-03-12 10:39:21 +0100

    [CORE] Firebase handler has the responsability of manage notifications

--- Hamp/Hamp/AppDelegate.swift
+++ Hamp/Hamp/AppDelegate.swift
@@ -17,60 +17,31 @@ import FirebaseMessaging
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	var window: UIWindow?
+	private let remoteNotificationsHandler = RemoteNotificationsHandler()
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		
-		FirebaseApp.configure()
+		remoteNotificationsHandler.configure()
 		FabricManager.configure()
-		
-		Messaging.messaging().delegate = self
-		Messaging.messaging().shouldEstablishDirectChannel = true
 		
 		let _ = ProvidersManager.sharedInstance
 		ProvidersManager.sharedInstance.hampDataManager.printDataBaseFileRoute()
 		
 		showMainViewController()
 		
-		UNUserNotificationCenter.current().delegate = self
-		
-		let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-		UNUserNotificationCenter.current().requestAuthorization(
-			options: authOptions,
-			completionHandler: {_, _ in })
-		
-		application.registerForRemoteNotifications()
-		
 		return true
 	}
 	
 	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-		Logger.d("Registered for remote notifications")
+		remoteNotificationsHandler.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
 	}
 	
 	func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-		Logger.d(userInfo)
-		completionHandler(.noData)
+		remoteNotificationsHandler.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
 	}
 	
 	func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-		print("Failed to register remote notification \(error.localizedDescription)")
-		Logger.d(error.localizedDescription, event: .e)
-	}
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
-	// Firebase push
-	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-		print("Push received: \(response.notification.request.content.body)")
-		completionHandler()
-	}
-	
-	func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
-		Logger.d("Token received: \(fcmToken)")
-	}
-	
-	func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
-		Logger.d("Message received: \(remoteMessage.appData)")
+		remoteNotificationsHandler.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
 	}
 }
 
---
+++ Hamp/Hamp/Utils/Firebase/RemoteNotificationsHandler.swift
@@ -0,0 +1,72 @@
+//
+//  RemoteNotificationsHandler.swift
+//  Hamp
+//
+//  Created by Joan Molinas Ramon on 12/3/18.
+//  Copyright © 2018 com.hamp.hampiOS. All rights reserved.
+//
+
+import Firebase
+import UserNotifications
+
+class RemoteNotificationsHandler: NSObject {
+	
+	private var configured = false
+	
+	// MARK: - Public api
+	func configure() {
+		guard !configured else { return }
+		
+		FirebaseApp.configure()
+		Messaging.messaging().delegate = self
+		configureNotifications()
+		configured = true
+	}
+}
+
+extension RemoteNotificationsHandler {
+	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+		Logger.d("Registered for remote notifications")
+	}
+	
+	func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+		Logger.d(userInfo)
+		completionHandler(.noData)
+	}
+	
+	func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+		print("Failed to register remote notification \(error.localizedDescription)")
+		Logger.d(error.localizedDescription, event: .e)
+	}
+}
+
+private extension RemoteNotificationsHandler {
+	func configureNotifications() {
+		UNUserNotificationCenter.current().delegate = self
+		
+		let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+		UNUserNotificationCenter.current().requestAuthorization(
+			options: authOptions,
+			completionHandler: {_, _ in })
+		
+		UIApplication.shared.registerForRemoteNotifications()
+	}
+}
+
+extension RemoteNotificationsHandler: UNUserNotificationCenterDelegate {
+	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+		print("Push received: \(response.notification.request.content.body)")
+		completionHandler()
+	}
+}
+
+extension RemoteNotificationsHandler: MessagingDelegate {
+	func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+		Logger.d("Token received: \(fcmToken)")
+	}
+	
+	func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+		Logger.d("Message received: \(remoteMessage.appData)")
+	}
+}
+
